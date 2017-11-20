6e61faaf9b33dfcd8d4d8be9e790674b8fb40a7d
stiv15 <stivmarquez.ud@gmail.com>
2017-11-20 11:47:46 -0500

    Desarrollo openNote 	* Adición libreria FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,18 @@ import { HomePage } from '../pages/home/home';
 import { NotasService } from '../services/notas.service';
 import { DetallePage } from '../pages/detalle/detalle';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDurCoVNqQQYbYTNLtCzXyewlK94cXBFmM",
+  authDomain: "noteopen-2b8f9.firebaseapp.com",
+  databaseURL: "https://noteopen-2b8f9.firebaseio.com",
+  storageBucket: "noteopen-2b8f9.appspot.com",
+  messagingSenderId: "86269464477"
+};
 
 
 @NgModule({
@@ -19,7 +31,10 @@ import { DetallePage } from '../pages/detalle/detalle';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
